73e14832d6208ccabb5821ad54e81fda386fdc6d
Makena-Maryann <maryann.makena00@gmail.com>
2020-06-14 13:05:45 +0300

    Add postDate property in our quote constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,7 +4,8 @@ export class Quote {
   constructor(
     public statement: string,
     public author: string,
-    public name: string
+    public name: string,
+    public postDate: Date
   ) {
     this.upVote = 0;
     this.downVote = 0;
