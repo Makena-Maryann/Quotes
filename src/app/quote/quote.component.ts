12f4eca99e050d382a06e5ac3bded5298d51cd35
Makena-Maryann <maryann.makena00@gmail.com>
2020-06-15 23:11:46 +0300

    Reduce number of quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,6 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(
-      'I can’t make people not afraid of black people. I don’t know what’s going on. I can’t explain what’s happening in your head. But maybe if I show up every day as a human, a good human, doing wonderful things, loving my family, loving your kids, taking care of things that I care about—maybe, just maybe that work will pick away at the scabs of your discrimination. Maybe that slowly will unravel it. That’s all we have, because we can’t do it for them, because they’re broken. Their brokenness in how they see us is a reflection of this brokenness. And you can’t fix that. All you can do is the work.',
-      'Michelle Obama',
-      'Melissa',
-      new Date(2020, 5, 11)
-    ),
-    new Quote(
       'No matter how big a nation is, it is no stronger than its weakest people, and as long as you keep a person down, some part of you has to be down there to hold him down, so it means you cannot soar as you might otherwise.',
       'Marian Anderson',
       'Christopher',
@@ -25,12 +19,6 @@ export class QuoteComponent implements OnInit {
       'Roxane Gay',
       'Larry',
       new Date(2020, 5, 13)
-    ),
-    new Quote(
-      'The Black female is assaulted in her tender years by all those common forces of nature at the same time that she is caught in the tripartite crossfire of masculine prejudice, white illogical hate and Black lack of power. The fact that the adult American Negro female emerges a formidable character is often met with amazement, distaste and even belligerence. It is seldom accepted as an inevitable outcome of the struggle won by survivors and deserves respect if not enthusiastic acceptance.',
-      'Maya Angelou',
-      'Christine',
-      new Date(2020, 5, 14)
     ),
   ];
 
